e5f83b183d00e97483cbc8f922c28ceb0fec16ab
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-31 22:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function onSubmit() {
+      var form = document.getElementById('myForm');
+      var alertText = '';
+      for (var i = 0; i < form.elements.length; i++) {
+        if (form.elements[i].name) {
+          alertText += form.elements[i].name + ': ' + form.elements[i].value + ' ';
+        }
+      }
+      alert(alertText);
+    }
